2774676975862379734fbc40770e379d1181071c
RaeVaughan <rachelev.tx@gmail.com>
2017-06-27 22:09:17 -0500

    npm packages installed, d, constructors in place

---
+++ BasicCard.js
@@ -0,0 +1,10 @@
+function BasicCard(question, answer) {
+	this.question = question;
+	this.answer = answer;
+}
+
+
+
+console.log("basic cards working");
+
+//module.exports = BasicCard;
---
+++ ClozeCard.js
@@ -0,0 +1,13 @@
+// The constructor should throw or log an error when the cloze deletion does not appear in the input text.
+
+function ClozeCard(cloze, partial, full) {
+	this.cloze = cloze;
+	this.partial = partial;
+	this.full = full;
+}
+
+
+
+console.log("cloze cards working");
+
+//module.exports = ClozeCard;
